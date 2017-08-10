1dd9d34a00ec56c409e7580f8ab9eff91bfa8a75
jeekmr <30857835+jeekmr@users.noreply.github.com>
2017-08-10 13:36:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,14 @@ var element= document.getElementById('para');
 element.innerHTML='new value';
 
 var img=document.getElementById('madi');
+
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft +'px';
+}
+
 img.onclick=function(){
-    img.style.marginleft='100px';
+    var interval=setInterval(moveRight,100);
+   
 };
