cc03dca98ae804d7161f704c62bdc2f5eb7847d2
jeekmr <30857835+jeekmr@users.noreply.github.com>
2017-08-10 13:22:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element= document.getElementById('para');
 element.innerHTML='new value';
 
-var img=document.getElementById('log');
+var img=document.getElementById('madi');
 img.onclick=function(){
     img.style.marginleft='100px';
 }
