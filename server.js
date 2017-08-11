da41367072216756a714625bb0289b7a6ac3a207
jeekmr <30857835+jeekmr@users.noreply.github.com>
2017-08-11 10:25:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,6 +49,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {counter =counter=1;
+  res.send(counter.toString());
+});
+
 
 app.get('/article1', function (req, res) {
   res.send(createTemplte(article1));
