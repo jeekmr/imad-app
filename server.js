9775391baefa196434576c823621d2468ddc2368
jeekmr <30857835+jeekmr@users.noreply.github.com>
2017-08-22 11:54:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/profile.jpg', function (req, res) {
 var names=[];
 app.get('/submit-name/:name',function(req,res){
    
-   var name= req.paras.name;
+   var name= req.params.name;
    
    names.push(name);
    
