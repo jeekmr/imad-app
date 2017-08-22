39a612c0b1c40fbf97232369cfcf8ac38486236f
jeekmr <30857835+jeekmr@users.noreply.github.com>
2017-08-22 10:18:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,38 @@ button.onclick=function(){
     span.innerHTML=counter.toString();
     
 }; 
+
+//listing names
+
+var nameInput =document.getElementById('name');
+var name =nameInput.value;
+var submit= document.getElementById('submitbutton')
+submit.onclick=function(){ 
+    
+    
+    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<=names.length;i++){
+        lsit += '<li>'+ names[i]+ '</li>'
+        
+        var ul=document.getElementById('listname');
+        ul.innerHTML=list;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
